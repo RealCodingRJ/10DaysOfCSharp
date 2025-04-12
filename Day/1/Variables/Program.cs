44fbb7a36b87a67f24220e4ecef08cf18df68c01
Ryan James <realcoder2023@gmail.com>
2025-04-12 16:10:18 -0400

    added day one learn

---
+++ Day/1/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+
+    class Program
+    {
+        public static void Main(string[] _)
+        {
+            String? name = "Ryan";
+            if (name is not null) return;
+
+            int? age = 25;
+
+            if (age is not null) return;
+
+            bool? isAdmin = true;
+            if (isAdmin is not null) return;
+        }
+    }
+
+}
